7855efc895092193d7624be865ade1bbf6ccbe7c
cgp <cgp@dasburo.com>
2025-03-29 14:51:52 +0100

    Uncomment awards

--- components/sections/awards/awards.tsx
+++ components/sections/awards/awards.tsx
@@ -1,30 +1,30 @@
-'use client'
+// 'use client'
 
-import content from '@/data/content.json'
+// import content from '@/data/content.json'
 
-export const Awards = () => {
-  return (
-    <section className='py-14'>
-      <h2 className='text-3xl font-bold tracking-tight mb-12'>
-        {content.awards.title}
-      </h2>
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-        {content.awards.items.map((award, index) => (
-          <a
-            key={index}
-            href={award.link}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='block'
-          >
-            <div className='border border-mint/20 rounded-lg p-6 h-full transition-colors hover:border-mint hover:bg-mint/5 bg-background'>
-              <div className='text-muted-foreground mb-2'>{award.year}</div>
-              <h3 className='font-semibold text-xl mb-2'>{award.title}</h3>
-              <p>{award.project}</p>
-            </div>
-          </a>
-        ))}
-      </div>
-    </section>
-  )
-}
+// export const Awards = () => {
+//   return (
+//     <section className='py-14'>
+//       <h2 className='text-3xl font-bold tracking-tight mb-12'>
+//         {content.awards.title}
+//       </h2>
+//       <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+//         {content.awards.items.map((award, index) => (
+//           <a
+//             key={index}
+//             href={award.link}
+//             target='_blank'
+//             rel='noopener noreferrer'
+//             className='block'
+//           >
+//             <div className='border border-mint/20 rounded-lg p-6 h-full transition-colors hover:border-mint hover:bg-mint/5 bg-background'>
+//               <div className='text-muted-foreground mb-2'>{award.year}</div>
+//               <h3 className='font-semibold text-xl mb-2'>{award.title}</h3>
+//               <p>{award.project}</p>
+//             </div>
+//           </a>
+//         ))}
+//       </div>
+//     </section>
+//   )
+// }
