4efc9dacd7c408926604e6d46bb3dc55bcdd96f9
cgp <gjelstrup78@gmail.com>
2025-07-15 12:47:23 +0200

    Enhance font configuration by adding 'display: swap' property for normal and bold fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,11 +3,13 @@ import localFont from 'next/font/local'
 export const fontNormal = localFont({
   src: './mono-normal.woff2',
   variable: '--font-normal',
-  preload: true
+  preload: true,
+  display: 'swap'
 })
 
 export const fontBold = localFont({
   src: './mono-bold.woff2',
   variable: '--font-bold',
-  preload: true
+  preload: true,
+  display: 'swap'
 })
