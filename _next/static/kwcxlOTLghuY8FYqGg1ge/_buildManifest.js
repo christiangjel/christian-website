d85592d8989780a943d6b51e08138d85a29616a9
cgp <gjelstrup78@gmail.com>
2025-12-29 16:52:07 +0100

    Updates

---
+++ _next/static/kwcxlOTLghuY8FYqGg1ge/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/christian-website//_next/:path+",
+        "destination": "/christian-website/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
